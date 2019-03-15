ec62fe2de9b32a7d4c932451d9ab38ad47ae0b6d
ChrisPschibila <christopher.pschibila@web.de>
2019-03-15 12:41:34 +0100

    Änderungen an dem Enum Category

--- src/main/java/de/dhbw/vote/jpa/Category.java
+++ src/main/java/de/dhbw/vote/jpa/Category.java
@@ -9,6 +9,26 @@ package de.dhbw.vote.jpa;
  *
  * @author chp
  */
-public class Category {
+public enum Category {
+    HOT_OR_NOT, AUTO, ESSEN, SPORT, UNBEKANNT;
     
+    /**
+     * Bezeichnung ermitteln
+     *
+     * @return Bezeichnung
+     */
+    public String getLabel() {
+        switch (this) {
+            case HOT_OR_NOT:
+                return "Hot or Not";
+            case AUTO:
+                return "Auto";
+            case ESSEN:
+                return "Essen";
+            case SPORT:
+                return "Sport";
+            default:
+                return this.toString();
+        }
+    }
 }
